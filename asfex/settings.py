22ba78350ebe5055e2cc9f879fc87f3598773f10
f3l1x235 <ziakrabaservice@gmail.com>
2026-09-17 10:13:51 +0100

    updated

--- asfex/settings.py
+++ asfex/settings.py
@@ -159,6 +159,9 @@ STORAGES = {
     },
 }
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
 
--- asfex/urls.py
+++ asfex/urls.py
@@ -1,5 +1,7 @@
 from django.shortcuts import redirect
 from django.urls import include, path
+from . import settings
+from django.conf.urls.static import static
 
 
 def admin_redirect(request):
@@ -9,4 +11,5 @@ def admin_redirect(request):
 urlpatterns = [
     path('admin/', admin_redirect, name='admin_redirect'),
     path('', include('siteapp.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
