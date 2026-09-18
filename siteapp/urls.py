4dbecd4a2783658316ac5f6b5a279f90116a7cd6
f3l1x235 <ziakrabaservice@gmail.com>
2026-09-18 10:41:45 +0100

    updated urls

--- siteapp/urls.py
+++ siteapp/urls.py
@@ -47,8 +47,8 @@ urlpatterns = [
     path('robots.txt', views.robots_txt, name='robots_txt'),
     path('sitemap.xml', views.sitemap_xml, name='sitemap_xml'),
     path('gestion/partenaires/', views.admin_partners, name='admin_partners'),
-    path('gestion/partenaires/nouveau/', views.admin_partner_new, name='admin_partner_new'),
-    path('gestion/partenaires/<int:pk>/modifier/', views.admin_partner_edit, name='admin_partner_edit'),
-    path('gestion/partenaires/<int:pk>/supprimer/', views.admin_partner_delete, name='admin_partner_delete'),
-    
+    path('gestion/partenaires/nouveau/', views.admin_partners_new, name='admin_partner_new'),
+    path('gestion/partenaires/<int:pk>/modifier/', views.Admin_partners_edit, name='admin_partner_edit'),
+    path('gestion/partenaires/<int:pk>/supprimer/', views.Admin_partners_delete, name='admin_partner_delete'),
+
 ]
