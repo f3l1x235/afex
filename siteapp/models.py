51389da7f4bf19f2a036e868f8697a0652e01a0c
f3l1x235 <ziakrabaservice@gmail.com>
2026-09-18 11:02:46 +0100

    updated urls

--- siteapp/models.py
+++ siteapp/models.py
@@ -16,7 +16,7 @@ class Category(models.Model):
 class Partner(models.Model):
     name = models.CharField(max_length=150)
     logo = models.ImageField(upload_to='partners/')
-    website = models.URLField(blank=True)
+    website = models.URLField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
